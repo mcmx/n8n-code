cdcb337e4375fcf1b2a872a22d1fd8bb2df4b0d3
ricardo <ricardoespinoza105@gmail.com>
2022-03-31 17:41:52 -0400

    aja

--- packages/cli/src/PublicApi/helpers.ts
+++ packages/cli/src/PublicApi/helpers.ts
@@ -1,12 +1,23 @@
 import * as querystring from 'querystring';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { pick } from 'lodash';
+import express = require('express');
+import * as SwaggerParser from '@apidevtools/swagger-parser';
+// eslint-disable-next-line import/no-cycle
 import { User } from '../databases/entities/User';
 
 interface IPaginationOffsetDecoded {
 	offset: number;
 	limit: number;
 }
+export interface IMiddlewares {
+	[key: string]: [IMiddleware];
+}
+interface IMiddleware {
+	(req: express.Request, res: express.Response, next: express.NextFunction): void;
+}
+
+export type OperationID = 'getUsers' | 'getUser';
 
 export const decodeCursor = (cursor: string): IPaginationOffsetDecoded => {
 	const data = JSON.parse(Buffer.from(cursor, 'base64').toString()) as string;
@@ -53,4 +64,52 @@ export const clean = (users: User[], keepRole = false): Array<Partial<User>> => 
 	return users.map((user) =>
 		pick(user, getSelectableProperties('user').concat(keepRole ? ['globalRole'] : [])),
 	);
+};
+
+const middlewareDefined = (operationId: OperationID, middlewares: IMiddlewares) =>
+	operationId && middlewares[operationId];
+
+export const addMiddlewares = (
+	router: express.Router,
+	method: string,
+	routePath: string,
+	operationId: OperationID,
+	middlewares: IMiddlewares,
+): void => {
+	if (middlewareDefined(operationId, middlewares)) {
+		routePath.replace(/\{([^}]+)}/g, ':$1');
+		switch (method) {
+			case 'get':
+				router.get(routePath, ...middlewares[operationId]);
+				break;
+			case 'post':
+				router.post(routePath, ...middlewares[operationId]);
+				break;
+			case 'put':
+				router.post(routePath, ...middlewares[operationId]);
+				break;
+			case 'delete':
+				router.post(routePath, ...middlewares[operationId]);
+				break;
+			default:
+				break;
+		}
+	}
+};
+export const addCustomMiddlewares = async (
+	apiController: express.Router,
+	openApiSpec: string,
+	middlewares: IMiddlewares,
+): Promise<void> => {
+	const { paths = {} } = await SwaggerParser.parse(openApiSpec);
+	Object.entries(paths).forEach(([routePath, methods]) => {
+		Object.entries(methods).forEach(([method, data]) => {
+			const operationId: OperationID = (
+				data as {
+					'x-eov-operation-id': OperationID;
+				}
+			)['x-eov-operation-id'];
+			addMiddlewares(apiController, method, routePath, operationId, middlewares);
+		});
+	});
 };
--- packages/cli/src/PublicApi/middlewares.ts
+++ packages/cli/src/PublicApi/middlewares.ts
@@ -1,12 +1,39 @@
-import { NextFunction, Request, Response } from 'express';
+import express = require('express');
+import config = require('../../config');
 
-const sayHello = (req: Request, res: Response, next: NextFunction): void => {
-	console.log('hello');
-	console.log('se llamo esta vegra');
-	next();
+type Role = 'owner' | 'member';
+
+const instanceOwnerSetup = (
+	req: express.Request,
+	res: express.Response,
+	next: express.NextFunction,
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+): any => {
+	if (!config.get('userManagement.isInstanceOwnerSetUp')) {
+		return res.status(400).json({ message: 'asasas' });
+	}
+	return next();
 };
 
+const authorize =
+	(role: [Role]) =>
+	(
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction,
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	): any => {
+		const {
+			globalRole: { name: userRole },
+		} = req.user as { globalRole: { name: Role } };
+		if (!role.includes(userRole)) {
+			return res.status(400).json({ message: 'asasas' });
+		}
+		return next();
+	};
+
 export const middlewares = {
-	getUsers: [sayHello],
-	getUser: [sayHello],
+	createUsers: [instanceOwnerSetup, authorize(['owner'])],
+	getUsers: [instanceOwnerSetup, authorize(['owner'])],
+	getUser: [instanceOwnerSetup, authorize(['owner'])],
 };
